5d90a268450d1b03391f1026bd9bb4cb2ee48f9d
yogijagdish <yogijagdish007@gmail.com>
2023-02-11 08:40:11 +0545

    project started

---
+++ README.md
@@ -0,0 +1,3 @@
+## Decentralized lottery
+
+<p> This is a decentralized lottery project based on blochchain and worked on etherium. </p>
---
+++ contracts/Lottery.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.6;
+
+contract Lottery {
+    function startLottery() public {}
+    function endLottery() public{}
+    function entranceFee() public{}
+    function calculateEntrance() public{}
+}
