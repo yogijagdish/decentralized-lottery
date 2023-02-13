3132113ac1e719e9ad5d00e2d08c36ebad62c3f5
yogijagdish <yogijagdish007@gmail.com>
2023-02-13 15:28:17 +0545

    smart contract written

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,14 +1,17 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract Lottery {
+contract Lottery is Ownable {
 
     address payable[] public players;
     uint256 entranceRate = 50 * 10 ** 18;
     AggregatorV3Interface public ethUSDPriceFeed;
+    uint256 randomNumber;
+    address payable public winner;
 
     enum LotteryState {
         OPEN,
@@ -18,7 +21,7 @@ contract Lottery {
 
     LotteryState public lotteryState;
 
-    constructor(address _priceFeed) {
+    constructor (address _priceFeed) public {
         lotteryState = LotteryState.CLOSED;
         ethUSDPriceFeed = AggregatorV3Interface(_priceFeed);
     }
@@ -27,7 +30,20 @@ contract Lottery {
         require(msg.value > entranceFee(),"sorry not enough fees");
         players.push(payable(msg.sender));
     }
-    function endLottery() public{}
+    function endLottery() public onlyOwner{
+        randomNumber = uint256(
+            keccak256(
+                abi.encodePacked(
+                    msg.sender,
+                    block.difficulty,
+                    block.timestamp
+                )
+            )
+        ) % players.length;
+        winner = players[randomNumber];
+        winner.transfer(address(this).balance);
+        players = new address payable[](0);
+    }
     function entranceFee() public view returns(uint256){
         (,int256 answer,,,) = ethUSDPriceFeed.latestRoundData();
         uint256 adjustedPrice = uint256(answer) * 10 ** 10;
@@ -35,9 +51,8 @@ contract Lottery {
         return entrancePrice;
         
     }
-    function startLottery() public{
+    function startLottery() public onlyOwner{
         require(lotteryState == LotteryState.CLOSED,"cannot start new lottery yet!!!");
         lotteryState = LotteryState.OPEN;
-
     }
 }
