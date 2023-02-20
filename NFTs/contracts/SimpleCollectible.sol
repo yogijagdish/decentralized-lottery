34a0b7ace3df8730c44adbea686ee6c02419c375
yogijagdish <yogijagdish007@gmail.com>
2023-02-20 11:31:28 +0545

    initial project compiled

--- NFTs/contracts/SimpleCollectible.sol
+++ NFTs/contracts/SimpleCollectible.sol
@@ -1,7 +1,10 @@
-// SPDX-Lisence-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract SimpleCollectible is ERC721 {}
+contract SimpleCollectible is ERC721 {
+
+    constructor() public ERC721("Doggie","dog") {}
+}
